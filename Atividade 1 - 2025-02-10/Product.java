4ebc77da093cd421f262327cafc722675d1c9eaa
alan-diogo <alandiogo9@gmail.com>
2025-02-17 01:36:54 -0300

    product

---
+++ Atividade 1 - 2025-02-10/Product.java
@@ -0,0 +1,20 @@
+public class Product {
+
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    String getName(){
+        return name;
+    }
+    double getPrice(){
+        return price;
+    }
+    @Override
+    public String toString() {
+        return "Product name: " + name + ", Price: " + price;
+    }
+}
