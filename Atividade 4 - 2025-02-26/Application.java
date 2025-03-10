a352e610666189266e61bada5de79f3056817a28
alan-diogo <alandiogo9@gmail.com>
2025-03-09 23:59:32 -0300

    Create Application.java

---
+++ Atividade 4 - 2025-02-26/Application.java
@@ -0,0 +1,33 @@
+package Application;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Holder: ");
+        String holder = sc.nextLine();
+        System.out.print("Initial balance: ");
+        double initialBalance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account account = new Account(number, holder, initialBalance, withdrawLimit);
+
+        System.out.print("Enter amount for withdraw: ");
+        double amount = sc.nextDouble();
+
+        try {
+            account.withdraw(amount);
+            System.out.printf("New balance: %.2f%n", account.getBalance());
+        } catch (WithdrawException e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
+    }
+}
